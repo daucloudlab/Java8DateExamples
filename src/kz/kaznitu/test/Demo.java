1395552da11f7bae9253e81ed831e81b3be883d3
daulet <daucloudlab@gmail.com>
2016-10-06 15:36:02 +0600

    version 01

---
+++ src/kz/kaznitu/test/Demo.java
@@ -0,0 +1,11 @@
+package kz.kaznitu.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Demo {
+    public static void main(String[] args) {
+        LocalDateTest.test();
+
+    }
+}
---
+++ src/kz/kaznitu/test/LocalDateTest.java
@@ -0,0 +1,24 @@
+package kz.kaznitu.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class LocalDateTest {
+    public static void test(){
+        // the current date
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Қазіргі уақыт: " + currentDate);
+
+        // 2014-02-10
+        LocalDate tenthFeb2014 = LocalDate.of(2014, Month.FEBRUARY, 10);
+        System.out.println("2014 Ақпанның 10: " + tenthFeb2014);
+
+        // months values start at 1 (2014-08-01)
+        LocalDate firstAug2014 = LocalDate.of(2014, 8, 1);
+        System.out.println("2014 Тамыздың бірі: " + firstAug2014);
+
+        // the 65th day of 2010 (2010-03-06)
+        LocalDate sixtyFifthDayOf2010 = LocalDate.ofYearDay(2010, 65);
+        System.out.println("2010 жылдың 65-ші күні: " + sixtyFifthDayOf2010);
+    }
+}
